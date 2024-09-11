4e59d672fdeaebb010951f3f8a3894bdf7d6f9ff
aravind321o <151174359+aravind321o@users.noreply.github.com>
2024-09-11 20:18:28 +0530

    Time: 56 ms (5.79%), Space: 16.7 MB (74.98%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str,) -> str:
+        sys.set_int_max_str_digits(68000)
+        a,b=0,0
+        r,s=0,0
+        for i in range(len(num1)):
+            r = ord(num1[i])-48
+            a=a*10 + r
+        for j in range(len(num2)):
+            s = ord(num2[j])-48
+            b=b*10 +s
+        
+        return str(a+b)
